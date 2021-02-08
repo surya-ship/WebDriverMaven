b9453c970a5a2872e4d1f47bc744ebba6e0786d6
surya <leverage.surya@gmail.com>
2021-02-08 23:26:52 +0530

    Initial Commit

---
+++ src/test/java/webdriverMavenGit/LoginTest.java
@@ -0,0 +1,40 @@
+package webdriverMavenGit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		
+		WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();				
+		
+	}
+	@Test
+	public void doLogin() 
+	{
+		driver.get("http://gmail.com");	
+		driver.findElement(By.id("identifierId")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.xpath("//*[@id='identifierNext']/div/button/div[2]")).click();
+		
+		
+	}
+	@AfterSuite
+	public void tearDown() {
+		//driver.quit();
+	
+
+}
+}
